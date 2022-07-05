877223a3b633f66e490071215c8fc3933f2868f6
lesenelir <miaomiaobabyzy@gmail.com>
2022-07-05 21:03:26 +0800

    setup basic html css layout

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+import VideoCard from "./components/VideoCard/VideoCard"
+
+function App() {
+  return (
+    <div className="App">
+      <header>12</header>
+      <div className={"bottom"}>
+        <aside>
+          12
+        </aside>
+        <main>
+          <VideoCard/>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/VideoCard/VideoCard.jsx
@@ -0,0 +1,24 @@
+import './VideoCard.css'
+
+// React 引入图片要以这种形式引入
+import picture1 from '../../images/list-item/1.png'
+import profile_picture1 from '../../images/profile/1.jpg'
+
+
+const VideoCard = () => {
+  return (
+      <div className={"video-box"}>
+        <img src={picture1} alt="" className={"video-main-img"}/>
+        <div className={"video-down"}>
+          <img src={profile_picture1} alt="" className={"video-down-img"}/>
+          <div className={"video-down-right"}>
+            <p className={"p-header"}>Build and deploy a profile website</p>
+            <p className={"p-footer"}>freeCodeCamp.org</p>
+            <p className={"p-footer"}>71万次观看 · 2年前</p>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default VideoCard
